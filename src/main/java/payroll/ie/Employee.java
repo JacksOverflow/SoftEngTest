85287fe190dccfcf7d1db9733c2ccd88fe26afd6
ROBERT WEBBER - STUDENT <G00365406@gmit.ie>
2022-03-08 10:33:27 +0000

    employeeList class test finalized

--- src/main/java/payroll/ie/Employee.java
+++ src/main/java/payroll/ie/Employee.java
@@ -1,58 +1,70 @@
 package payroll.ie;
 
-public class Employee {
+public class Employee{
     String title;
     String name;
     int ppsID;
     int phone;
     String employmentType;
-    int Age;
+    int age;
+
+    Employee(String title, String name, int ppsID, int phone, String employmentType, int age) throws Exception {
+        if(title == "Mr" || title == "Mrs" || title == "Ms"){
+            this.title = title;
+        } else{
+            throw new IllegalArgumentException("Invalid Title... must be Mr,Mrs,Ms");
+        }
+        if(name.length() >= 5 && name.length() <= 22){
+            this.name = name;
+        }else{
+            throw new IllegalArgumentException("Invalid Name... length must be between 5 and 22 characters");
+        }
+        if(String.valueOf(ppsID).length() == 6){
+            this.ppsID = ppsID;
+        }else{
+            throw new IllegalArgumentException("Invalid PPS ID... must be 6 digits");
+        }
+        if(String.valueOf(phone).length() == 7){
+            this.phone = phone;
+        }else{
+            throw new IllegalArgumentException("Invalid Phone Number... must be 7 digits");
+        }
+        if(employmentType.equalsIgnoreCase("Full-time") || employmentType.equalsIgnoreCase("Part-time")){
+            this.employmentType = employmentType;
+        } else{
+            throw new IllegalArgumentException("Invalid Employment Type... must be 'Full-time' or 'Part-time'");
+        }
+
+        if(age >= 18){
+            this.age = age;
+        } else{
+            throw new IllegalArgumentException("Underage... must be 18+");
+        }
+
+    }
 
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getPpsID() {
         return ppsID;
-    }
-
-    public void setPpsID(int ppsID) {
-        this.ppsID = ppsID;
     }
 
     public int getPhone() {
         return phone;
     }
 
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
-
     public String getEmploymentType() {
         return employmentType;
     }
 
-    public void setEmploymentType(String employmentType) {
-        this.employmentType = employmentType;
-    }
-
     public int getAge() {
-        return Age;
+        return age;
     }
 
-    public void setAge(int age) {
-        Age = age;
-    }
 }
